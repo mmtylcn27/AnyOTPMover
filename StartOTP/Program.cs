86b0f39edcb0a7dec0ffad5ee60543c92c07afd2
Mehmet Yalçın <53427136+mmtylcn27@users.noreply.github.com>
2025-02-25 17:55:26 +0300

    Update Program.cs

--- StartOTP/Program.cs
+++ StartOTP/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Diagnostics;
+using System;
 using System.IO;
-using System.Runtime.ConstrainedExecution;
-using System.Runtime.InteropServices;
-using System.Security;
 using System.Text;
-
+using System.Runtime.InteropServices;
 
 namespace StartOTP
 {
@@ -14,35 +10,6 @@ namespace StartOTP
         private const string bAppName = "AnyOTP.exe";
         private const string bDllName = "HookWMIC.dll";
 
-        [Flags]
-        public enum ProcessAccessFlags : uint
-        {
-            All = 0x001F0FFF,
-            Terminate = 0x00000001,
-            CreateThread = 0x00000002,
-            VirtualMemoryOperation = 0x00000008,
-            VirtualMemoryRead = 0x00000010,
-            VirtualMemoryWrite = 0x00000020,
-            DuplicateHandle = 0x00000040,
-            CreateProcess = 0x000000080,
-            SetQuota = 0x00000100,
-            SetInformation = 0x00000200,
-            QueryInformation = 0x00000400,
-            QueryLimitedInformation = 0x00001000,
-            Synchronize = 0x00100000
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern IntPtr OpenProcess(
-        ProcessAccessFlags processAccess,
-        bool bInheritHandle,
-        int processId);
-        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
-        {
-            return OpenProcess(flags, false, proc.Id);
-        }
-
-        // VirtualAllocEx signture https://www.pinvoke.net/default.aspx/kernel32.virtualallocex
         [Flags]
         public enum AllocationType
         {
@@ -56,11 +23,6 @@ namespace StartOTP
             WriteWatch = 0x200000,
             LargePages = 0x20000000
         }
-
-        // VirtualFreeEx signture  https://www.pinvoke.net/default.aspx/kernel32.virtualfreeex
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress,
-        int dwSize, AllocationType dwFreeType);
 
         [Flags]
         public enum MemoryProtection
@@ -78,52 +40,72 @@ namespace StartOTP
             WriteCombineModifierflag = 0x400
         }
 
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern IntPtr VirtualAllocEx(
-            IntPtr hProcess,
-            IntPtr lpAddress,
-            IntPtr dwSize,
-            AllocationType flAllocationType,
-            MemoryProtection flProtect);
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct STARTUPINFO
+        {
+            public uint cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
+        }
+
+        const uint CREATE_SUSPENDED = 0x00000004;
+
+        // VirtualFreeEx signture  https://www.pinvoke.net/default.aspx/kernel32.virtualfreeex
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, AllocationType dwFreeType);
+
+        // VirtualAllocEx signture https://www.pinvoke.net/default.aspx/kernel32.virtualallocex
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
 
         // WriteProcessMemory signture https://www.pinvoke.net/default.aspx/kernel32/WriteProcessMemory.html
         [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(
-        IntPtr hProcess,
-        IntPtr lpBaseAddress,
-        [MarshalAs(UnmanagedType.AsAny)] object lpBuffer,
-        int dwSize,
-        out IntPtr lpNumberOfBytesWritten);
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [MarshalAs(UnmanagedType.AsAny)] object lpBuffer, int dwSize, out IntPtr lpNumberOfBytesWritten);
 
         // GetProcAddress signture https://www.pinvoke.net/default.aspx/kernel32.getprocaddress
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        [DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true)]
         static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
         // GetModuleHandle signture http://pinvoke.net/default.aspx/kernel32.GetModuleHandle
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
 
         // CreateRemoteThread signture https://www.pinvoke.net/default.aspx/kernel32.createremotethread
         [DllImport("kernel32.dll")]
-        static extern IntPtr CreateRemoteThread(
-        IntPtr hProcess,
-        IntPtr lpThreadAttributes,
-        uint dwStackSize,
-        IntPtr lpStartAddress,
-        IntPtr lpParameter,
-        uint dwCreationFlags,
-        IntPtr lpThreadId);
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
 
-        // CloseHandle signture https://www.pinvoke.net/default.aspx/kernel32.closehandle
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-        [SuppressUnmanagedCodeSecurity]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, IntPtr lpProcessAttributes, IntPtr lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
 
+        [DllImport("kernel32.dll")]
+        public static extern bool ResumeThread(IntPtr hThread);
 
         static void Main(string[] args)
         {
@@ -131,112 +113,112 @@ namespace StartOTP
             var bOtpPath = Path.Combine(bCurrentDir, bAppName);
             var bDllPath = Path.Combine(bCurrentDir, bDllName);
 
-            if (File.Exists(bOtpPath))
-            {
-                if (File.Exists(bDllPath))
-                {
-                    var bProcess = new Process();
-                    bProcess.StartInfo.FileName = bOtpPath;
-
-                    if (bProcess.Start())
-                    {
-                        var bPID = bProcess.Id;
-
-                        IntPtr ProcHandle = OpenProcess(
-                            ProcessAccessFlags.All,
-                            false,
-                            bPID);
-
-                        if (ProcHandle != null)
-                        {
-                            IntPtr DllSpace = VirtualAllocEx(
-                                ProcHandle,
-                                IntPtr.Zero,
-                                (IntPtr) bDllPath.Length,
-                                AllocationType.Reserve | AllocationType.Commit,
-                                MemoryProtection.ExecuteReadWrite);
-
-                            if (DllSpace != null)
-                            {
-                                byte[] bytes = Encoding.ASCII.GetBytes(bDllPath);
-
-                                bool DllWrite = WriteProcessMemory(
-                                    ProcHandle,
-                                    DllSpace,
-                                    bytes,
-                                    bytes.Length,
-                                    out var bytesread
-                                );
-
-                                if (DllWrite)
-                                {
-                                    IntPtr Kernel32Handle = GetModuleHandle("Kernel32.dll");
-                                    IntPtr LoadLibraryAAddress = GetProcAddress(Kernel32Handle, "LoadLibraryA");
-
-                                    IntPtr RemoteThreadHandle = CreateRemoteThread(
-                                        ProcHandle,
-                                        IntPtr.Zero,
-                                        0,
-                                        LoadLibraryAAddress,
-                                        DllSpace,
-                                        0,
-                                        IntPtr.Zero
-                                    );
-
-                                    if (RemoteThreadHandle != null)
-                                    {
-                                        WaitForSingleObject(RemoteThreadHandle, 0xFFFFFFFF);
-                                        VirtualFreeEx(ProcHandle, DllSpace, 0, AllocationType.Release);
-                                        CloseHandle(RemoteThreadHandle);
-                                        CloseHandle(ProcHandle);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine(
-                                            $"CreateRemoteThread Failed! Error: {Marshal.GetLastWin32Error()}");
-                                        VirtualFreeEx(ProcHandle, DllSpace, 0, AllocationType.Release);
-                                        CloseHandle(ProcHandle);
-                                        Console.ReadLine();
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine(
-                                        $"WriteProcessMemory Failed! Error: {Marshal.GetLastWin32Error()}");
-                                    VirtualFreeEx(ProcHandle, DllSpace, 0, AllocationType.Release);
-                                    CloseHandle(ProcHandle);
-                                    Console.ReadLine();
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine($"VirtualAllocEx Failed! Error: {Marshal.GetLastWin32Error()}");
-                                CloseHandle(ProcHandle);
-                                Console.ReadLine();
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine($"OpenProcess Failed! Error: {Marshal.GetLastWin32Error()}");
-                            Console.ReadLine();
-                        }
-                    }
-                    else
-                    {
-                        Console.Write("Hata-1");
-                        Console.ReadLine();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{bDllPath} Bulunamadı!");
-                    Console.ReadLine();
-                }
-            }
-            else
+            if (!File.Exists(bOtpPath))
             {
                 Console.WriteLine($"{bOtpPath} Bulunamadı!");
                 Console.ReadLine();
+                return;
+            }
+
+            if (!File.Exists(bDllPath))
+            {
+                Console.WriteLine($"{bDllPath} Bulunamadı!");
+                Console.ReadLine();
+                return;
+            }
+
+            var startupInfo = new STARTUPINFO();
+            startupInfo.cb = (uint)Marshal.SizeOf(startupInfo);
+
+            if (!CreateProcess(
+                    null, 
+                    bOtpPath,
+                    IntPtr.Zero,
+                    IntPtr.Zero,
+                    false,
+                    CREATE_SUSPENDED,
+                    IntPtr.Zero,
+                    null,
+                    ref startupInfo, 
+                    out var processInfo))
+            {
+                Console.WriteLine($"CreateProcess Failed! Error: {Marshal.GetLastWin32Error()}");
+                Console.ReadLine();
+                return;
+            }
+
+            var dllNameAddress = VirtualAllocEx(
+                processInfo.hProcess,
+                IntPtr.Zero,
+                (IntPtr) bDllPath.Length,
+                AllocationType.Reserve | AllocationType.Commit,
+                MemoryProtection.ExecuteReadWrite);
+
+            if (dllNameAddress == IntPtr.Zero)
+            {
+                Console.WriteLine($"VirtualAllocEx Failed! Error: {Marshal.GetLastWin32Error()}");
+                Console.ReadLine();
+                return;
+            }
+
+            try
+            {
+                var dllNameBytes = Encoding.ASCII.GetBytes(bDllPath);
+
+                if (!WriteProcessMemory(
+                        processInfo.hProcess,
+                        dllNameAddress,
+                        dllNameBytes,
+                        dllNameBytes.Length,
+                        out _
+                    ))
+                {
+                    Console.WriteLine($"WriteProcessMemory Failed! Error: {Marshal.GetLastWin32Error()}");
+                    Console.ReadLine();
+                    return;
+                }
+
+                var kernel32Address = GetModuleHandle("kernel32.dll");
+
+                if (kernel32Address == IntPtr.Zero)
+                {
+                    Console.WriteLine($"GetModuleHandle Failed! Error: {Marshal.GetLastWin32Error()}");
+                    Console.ReadLine();
+                    return;
+                }
+
+                var loadLibraryAddress = GetProcAddress(kernel32Address, "LoadLibraryA");
+
+                if (loadLibraryAddress == IntPtr.Zero)
+                {
+                    Console.WriteLine($"GetProcAddress Failed! Error: {Marshal.GetLastWin32Error()}");
+                    Console.ReadLine();
+                    return;
+                }
+
+                var hThread = CreateRemoteThread(
+                    processInfo.hProcess,
+                    IntPtr.Zero,
+                    0,
+                    loadLibraryAddress,
+                    dllNameAddress,
+                    0,
+                    IntPtr.Zero
+                );
+
+                if (hThread == IntPtr.Zero)
+                {
+                    Console.WriteLine($"CreateRemoteThread Failed! Error: {Marshal.GetLastWin32Error()}");
+                    Console.ReadLine();
+                    return;
+                }
+
+                WaitForSingleObject(hThread, 0xFFFFFFFF);
+                ResumeThread(processInfo.hThread);
+            }
+            finally
+            {
+                VirtualFreeEx(processInfo.hProcess, dllNameAddress, 0, AllocationType.Release);
             }
         }
     }
